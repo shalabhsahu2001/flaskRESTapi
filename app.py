769349e9d0ee966ae4488694075a039331fadb87
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-25 01:28:19 +0530

    flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,5 +8,17 @@ def welcome():
 @app.route("/home")
 def home():
     return "This is home page"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template(login.html)
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 from controller import user_controller, product_controller
